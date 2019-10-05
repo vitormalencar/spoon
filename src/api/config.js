34bad25c54dfc1a8e62a80a47ecfbd8402278685
vitormalencar <vitor.maia.alencar@gmail.com>
2019-10-04 23:56:18 -0300

    adding api config file

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+// @flow
+export const API_ENVIRONMENT_ID: string = "master";
+export const API_SPACE_ID: string = "kk2bw5ojx476";
+export const API_BASE_URL: string = "https://cdn.contentful.com";
+export const API_TOKEN: string =
+  "7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c";
