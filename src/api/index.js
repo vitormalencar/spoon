34d4b127b786409e7be89e5485918c28af2c6bb7
vitormalencar <vitor.maia.alencar@gmail.com>
2019-10-04 23:56:51 -0300

    adding  api file

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+// @flow
+import { createClient } from "contentful";
+import { API_SPACE_ID, API_TOKEN } from "./config";
+import { mapRecipe } from "../lib";
+
+const client = createClient({ space: API_SPACE_ID, accessToken: API_TOKEN });
+
+const recipeEntrie = {
+  content_type: "recipe",
+  select: "sys.id,fields"
+};
+
+export const fetchtRecipes = () =>
+  client
+    .getEntries(recipeEntrie)
+    .then(({ items }) => console.log({ items }) || items.map(mapRecipe));
