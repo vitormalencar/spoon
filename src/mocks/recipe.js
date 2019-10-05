7ad97c90aab4f64373415241defbf1594af79e20
vitormalencar <vitor.maia.alencar@gmail.com>
2019-10-05 18:37:58 -0300

    updating tests and adding mocks

--- src/mocks/recipe.js
+++ src/mocks/recipe.js
@@ -1,6 +1,6 @@
 export const recipeMock = {
   fields: {
-    title: "Crispy Chicken and Rice\twith Peas & Arugula Salad",
+    title: "Crispy Chicken and Rice with Peas & Arugula Salad",
     photo: {
       sys: {
         space: {
@@ -152,4 +152,16 @@ export const recipeMock = {
   sys: {
     id: "437eO3ORCME46i02SeCW46"
   }
+};
+
+export const mappedRecipe = {
+  title: "Crispy Chicken and Rice ",
+  subtitle: "with Peas & Arugula Salad",
+  tags: ["gluten free", "healthy"],
+  id: "437eO3ORCME46i02SeCW46",
+  chef: "Jony Chives",
+  url:
+    "//images.ctfassets.net/kk2bw5ojx476/5mFyTozvSoyE0Mqseoos86/fb88f4302cfd184492e548cde11a2555/SKU1479_Hero_077-71d8a07ff8e79abcb0e6c0ebf0f3b69c.jpg",
+  description:
+    "Crispy chicken skin, tender meat, and rich, tomatoey sauce form a winning trifecta of delicious in this one-pot braise. We spoon it over rice and peas to soak up every last drop of goodness, and serve a tangy arugula salad alongside for a vibrant boost of flavor and color. Dinner is served! Cook, relax, and enjoy!"
 };
